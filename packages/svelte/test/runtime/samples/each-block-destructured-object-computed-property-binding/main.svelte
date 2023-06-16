3b34862393179aae086eb99892a03041120e71eb
Nguyen Tran <nguyen.tran1072e@gmail.com>
2023-06-15 21:27:11 -0400

    Add computed property binding tests

---
+++ packages/svelte/test/runtime/samples/each-block-destructured-object-computed-property-binding/main.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let people;
+</script>
+
+{#each people as { ['name']: { ['fir' + 'st']: f, [true ? 'last' : 'lol']: l } } }
+	<input bind:value={f}>
+	<input bind:value={l}>
+	<p>{f} {l}</p>
+{/each}
